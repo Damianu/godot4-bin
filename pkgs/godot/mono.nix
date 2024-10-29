b173ff3e281c5ca03734dfc505d935fd3eb5cf3c
HotFish <admin@hot.fish>
2024-10-29 16:11:20 -0400

    Format with nix lsp formatter

--- pkgs/godot/mono.nix
+++ pkgs/godot/mono.nix
@@ -13,60 +13,62 @@
   arch ? "linux.x86_64",
   archWithUnderscore ? "linux_x86_64", # I have no comment on that..
 }:
-godotBin.overrideAttrs (oldAttrs: let
-  godotName = "Godot_v${version}_mono_${arch}";
-  godotNameUnderscore = "Godot_v${version}_mono_${archWithUnderscore}";
-in rec {
-  pname = "godot-mono";
+godotBin.overrideAttrs (
+  oldAttrs:
+  let
+    godotName = "Godot_v${version}_mono_${arch}";
+    godotNameUnderscore = "Godot_v${version}_mono_${archWithUnderscore}";
+  in
+  rec {
+    pname = "godot-mono";
 
-  src = fetchurl {
-    url = "https://github.com/godotengine/godot-builds/releases/download/${version}/Godot_v${version}_mono_${archWithUnderscore}.zip";
-    sha256 = versionHash;
-  };
+    src = fetchurl {
+      url = "https://github.com/godotengine/godot-builds/releases/download/${version}/Godot_v${version}_mono_${archWithUnderscore}.zip";
+      sha256 = versionHash;
+    };
 
-  buildInputs =
-    oldAttrs.buildInputs
-    ++ [
+    buildInputs = oldAttrs.buildInputs ++ [
       zlib
       msbuild
     ];
 
-  libraries = lib.makeLibraryPath buildInputs;
+    libraries = lib.makeLibraryPath buildInputs;
 
-  #Godot_v4.3-stable_mono_linux.x86_64
-  #Godot_v4.3-stable_mono_linux.x86_64
-  unpackPhase = ''
-    mkdir source
-    unzip $src -d source
-    mv source/${godotNameUnderscore}/* source
-    rmdir source/${godotNameUnderscore}
+    #Godot_v4.3-stable_mono_linux.x86_64
+    #Godot_v4.3-stable_mono_linux.x86_64
+    unpackPhase = ''
+      mkdir source
+      unzip $src -d source
+      mv source/${godotNameUnderscore}/* source
+      rmdir source/${godotNameUnderscore}
 
-  '';
-  installPhase = ''
-    mkdir -p $out/bin $out/opt/godot-mono
+    '';
+    installPhase = ''
+      mkdir -p $out/bin $out/opt/godot-mono
 
-    install -m 0755 source/${godotName} $out/opt/godot-mono/${godotName}
-    cp -r source/GodotSharp $out/opt/godot-mono
+      install -m 0755 source/${godotName} $out/opt/godot-mono/${godotName}
+      cp -r source/GodotSharp $out/opt/godot-mono
 
-    ln -s $out/opt/godot-mono/${godotName} $out/bin/godot-mono
+      ln -s $out/opt/godot-mono/${godotName} $out/bin/godot-mono
 
-    # Only create a desktop file, if the necessary variables are set
-    # these are set only, if one installs this program using flakes.
-    if [[ -f "${godotDesktopFile}" ]]; then
-      mkdir -p "$out/man/share/man/man6"
-      cp ${godotManpage} "$out/man/share/man/man6/"
+      # Only create a desktop file, if the necessary variables are set
+      # these are set only, if one installs this program using flakes.
+      if [[ -f "${godotDesktopFile}" ]]; then
+        mkdir -p "$out/man/share/man/man6"
+        cp ${godotManpage} "$out/man/share/man/man6/"
 
-      mkdir -p $out/share/{applications,icons/hicolor/scalable/apps}
-      cp ${godotDesktopFile} "$out/share/applications/org.godotengine.Godot-Mono.desktop"
-      cp ${godotIconSVG} "$out/share/icons/hicolor/scalable/apps/godot.svg"
-      cp ${godotIconPNG} "$out/share/icons/godot.png"
-      substituteInPlace "$out/share/applications/org.godotengine.Godot-Mono.desktop" \
-        --replace "Exec=godot" "Exec=$out/bin/godot-mono"
-    fi
-  '';
+        mkdir -p $out/share/{applications,icons/hicolor/scalable/apps}
+        cp ${godotDesktopFile} "$out/share/applications/org.godotengine.Godot-Mono.desktop"
+        cp ${godotIconSVG} "$out/share/icons/hicolor/scalable/apps/godot.svg"
+        cp ${godotIconPNG} "$out/share/icons/godot.png"
+        substituteInPlace "$out/share/applications/org.godotengine.Godot-Mono.desktop" \
+          --replace "Exec=godot" "Exec=$out/bin/godot-mono"
+      fi
+    '';
 
-  postFixup = ''
-    wrapProgram $out/bin/godot-mono \
-      --set LD_LIBRARY_PATH ${libraries}
-  '';
-})
+    postFixup = ''
+      wrapProgram $out/bin/godot-mono \
+        --set LD_LIBRARY_PATH ${libraries}
+    '';
+  }
+)
